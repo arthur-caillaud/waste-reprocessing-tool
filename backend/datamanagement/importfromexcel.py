2eadb4f19e6171acfa659b619b2ef55037ac567a
Mevel Pierre <pierre.mevel@student.ecp.fr>
2017-09-26 16:49:17 +0200

    Started the reading from excel python script/added excel test file

--- backend/datamanagement/importfromexcel.py
+++ backend/datamanagement/importfromexcel.py
@@ -1,4 +1,23 @@
 import xlrd
+import unittest
+
+class ExcelFile:
+    directory = "data/"
+    head_line_quantity = 2
+    starting_sheet = 0
+
+    def __init__(self, filename):
+        """Open excel file to read it"""
+        self.book = xlrd.open_workbook(self.directory + filename)
+        self.spreadsheet = self.book.sheet_by_index(self.starting_sheet)
+
+    def read_row_spreadsheet(self):
+        bordereaux = []
+        for i in range(self.head_line_quantity, self.spreadsheet.nrows):
+            bordereaux.append(self.spreadsheet.row_values(i))
+        return(bordereaux)
 
 
-def open_file(path):
+
+excel_file = ExcelFile("test.xlsx");
+print(excel_file.read_row_spreadsheet())
