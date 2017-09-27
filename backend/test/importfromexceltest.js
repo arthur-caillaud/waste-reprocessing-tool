13bcd08272b15644abb544b621dc883df0037564
Mevel Pierre <pierre.mevel@student.ecp.fr>
2017-09-27 17:29:10 +0200

    Managed to get to Importfromexcel in my test file for it.

--- backend/test/importfromexceltest.js
+++ backend/test/importfromexceltest.js
@@ -1,1 +1,8 @@
-var Excel = require('datamanagement/importfromexcel');
+var path = require('path');
+var Excel = require(path.join(__dirname, '../datamanagement', 'importfromexcel'));
+
+var test = require('tape');
+
+test('First test!', function (t) {
+    t.end();
+});
