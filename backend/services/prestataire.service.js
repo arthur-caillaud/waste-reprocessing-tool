5b667c58b5921b09cd0f780ed965b439abeea6a6
Mevel Pierre <pierre.mevel@student.ecp.fr>
2017-09-28 17:18:31 +0200

    Erased

--- backend/services/prestataire.service.js
+++ backend/services/prestataire.service.js
@@ -1,16 +1,6 @@
 'use strict';
-
-<<<<<<< HEAD
 var Rx = require('rx');
-var database = require('../database')
-
-=======
-var database = require('../database')
-var mongoose = require('mongoose');
-// var dataSchemas = require('./data/dataSchemas');
-// var Bordereau = mongoose.model('Bordereau', dataSchemas.bordereauSchema);
->>>>>>> 1edc45259386bcf0181dc723707f81c6f321c9b9
-
+var database = require('../database');
 var service = {};
 //All exported functionalities
 service.getAllPrestataires = getAllPrestataires;
@@ -19,13 +9,9 @@ module.exports = service;
 
 
 function getAllPrestataires() {
-<<<<<<< HEAD
-    
+
 }
 
 function getPrestataireByName(prestataireName){
 
-=======
-    return none
->>>>>>> 1edc45259386bcf0181dc723707f81c6f321c9b9
 }
