4d1d1d629ec35880d95595234982a1a17fde8c9d
Mevel Pierre <pierre.mevel@student.ecp.fr>
2017-09-28 11:46:33 +0200

    test completed

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var prestataires = require('./routes/prestataires')
 
 var app = express();
 
@@ -27,6 +28,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+app.use('/prestataires', prestataires);
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
