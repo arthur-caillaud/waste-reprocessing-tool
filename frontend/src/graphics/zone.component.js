0c18ac85b3ff9e69acb671f7d7b6e6965bc7059c
Aurélien Hugues <aurelien.huhues.59@gmail.com>
2017-10-03 11:58:31 +0200

    Add a component for displaying the graphics zone (currently a grey square)

---
+++ frontend/src/graphics/zone.component.js
@@ -0,0 +1,15 @@
+import React, { Component, } from 'react';
+
+import { Image } from 'react-bootstrap';
+
+class Zone extends Component {
+
+  render() {
+    return (
+      <Image src="../graphics/square.png" responsive/>
+    )
+  }
+}
+
+
+export default Zone;
