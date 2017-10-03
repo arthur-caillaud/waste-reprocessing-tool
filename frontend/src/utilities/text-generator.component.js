3a15a558519976d50dbb651f47c36f8df221d451
Aurélien Hugues <aurelien.huhues.59@gmail.com>
2017-10-03 11:59:00 +0200

    add a mock array to the text-generator utility component

--- frontend/src/utilities/text-generator.component.js
+++ frontend/src/utilities/text-generator.component.js
@@ -10,4 +10,9 @@ var longText = `
 
 var sentence = "Lorem ipsum dolor sit amet, ullum molestiae vituperata eum an.";
 
-export {longText, sentence};
+var array = [
+  "Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6",
+  "Item 7", "Item 8", "Item 9", "Item 10", "Item 11", "Item 12",
+];
+
+export {longText, sentence, array};
