3ed480410bf2e7bb8bffaae63064edc469612c81
Aurélien Hugues <aurelien.huhues.59@gmail.com>
2017-10-06 13:27:26 +0200

    Rename sample test to a more precise name

--- test/backend/server.js
+++ test/backend/server.js
@@ -5,7 +5,7 @@ var server = supertest.agent("http://localhost:4000");
 
 // UNIT test begin
 
-describe("SAMPLE unit test",function(){
+describe("Server",function(){
 
   // #1 should return home page
   it("should return home page",function(done){
