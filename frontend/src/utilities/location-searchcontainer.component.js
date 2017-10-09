cdbe9d4549d5eb0890f04731cd947a1f72cb9437
arthur-caillaud <arthurcaillaud@gmail.com>
2017-10-09 18:26:13 +0200

    Searchbar navbar

--- frontend/src/utilities/location-searchcontainer.component.js
+++ frontend/src/utilities/location-searchcontainer.component.js
@@ -7,18 +7,27 @@ import '../styles/location-search.css'
 class LocationSearchContainer extends Component {
     constructor(props) {
         super();
+        console.log("Props",props);
         this.state = {
-            data: props.searchContainerData,
+            data: props.data,
         }
     }
 
     render() {
         var searchContainer = [];
+        console.log("Rendering searchContainer");
         if(this.state.data){
+            console.log("Found data")
+            console.log("Data", this.state.data);
             this.state.data.forEach(searchResult => {
-                searchContainer.push(<ListGroupItem header={searchResult.value}>{searchResult.type}</ListGroupItem>)
+                searchContainer.push(
+                    <ListGroupItem header={searchResult.value}>
+                    {searchResult.type}
+                    </ListGroupItem>
+                );
             })
-        }
+        };
+        console.log(searchContainer);
         return (
             <ListGroup>
               {searchContainer}
