5b8167dd4c121c9cd7b339d569f196ae304f767d
Aurélien Hugues <aurelien.huhues.59@gmail.com>
2017-10-11 11:47:45 +0200

    Create Sequelize model for dashboard

---
+++ backend/models/dashboard.js
@@ -0,0 +1,62 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('dashboard', {
+    id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    nom: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    niveau: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false
+    },
+    ecarts_pesee: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      defaultValue: '0'
+    },
+    incoherences_filieres: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      defaultValue: '0'
+    },
+    filieres_interdites: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      defaultValue: '0'
+    },
+    retards: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      defaultValue: '0'
+    },
+    volume_total: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      defaultValue: '0'
+    },
+    taux_valorisation_total: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      defaultValue: '0'
+    },
+    taux_valorisation_l_verte: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      defaultValue: '0'
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: '0000-00-00'
+    }
+  }, {
+    tableName: 'dashboard'
+  });
+};
