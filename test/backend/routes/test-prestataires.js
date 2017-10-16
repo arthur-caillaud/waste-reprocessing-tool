966f8fb3a64870fa5526f558cc3445d140ce598c
Aurélien Hugues <aurelien.huhues.59@gmail.com>
2017-10-16 15:30:24 +0200

    Correct bugs in the queries Update tests for more routes

--- test/backend/routes/test-prestataires.js
+++ test/backend/routes/test-prestataires.js
@@ -13,7 +13,79 @@ describe("Prestataires Routes", () => {
     describe("GET /prestataires", () => {
 
         it("should return a 200 status code", (done) => {
-            server.get("/prestataires/new")
+            server.get("/prestataires/")
+                .then((response) => {
+                    assert.equal(response.status, 200);
+                    done();
+                })
+                .catch((err) => {
+                    done(err);
+                });
+        });
+
+        it("should return the correct data", () => {
+            // TODO as the data must be studied to be tested
+        })
+    })
+
+    describe("GET /prestataires?attributes=id,nom", () => {
+
+        it("should return a 200 status code", (done) => {
+            server.get("/prestataires/?attributes=id,nom")
+                .then((response) => {
+                    assert.equal(response.status, 200);
+                    done();
+                })
+                .catch((err) => {
+                    done(err);
+                });
+        });
+
+        it("should return the correct data", () => {
+            // TODO as the data must be studied to be tested
+        })
+    })
+
+    describe("GET /prestataires?order=-id", () => {
+
+        it("should return a 200 status code", (done) => {
+            server.get("/prestataires/?order=-id")
+                .then((response) => {
+                    assert.equal(response.status, 200);
+                    done();
+                })
+                .catch((err) => {
+                    done(err);
+                });
+        });
+
+        it("should return the correct data", () => {
+            // TODO as the data must be studied to be tested
+        })
+    })
+
+    describe("GET /prestataires?id=1", () => {
+
+        it("should return a 200 status code", (done) => {
+            server.get("/prestataires/?id=1")
+                .then((response) => {
+                    assert.equal(response.status, 200);
+                    done();
+                })
+                .catch((err) => {
+                    done(err);
+                });
+        });
+
+        it("should return the correct data", () => {
+            // TODO as the data must be studied to be tested
+        })
+    })
+
+    describe("GET /prestataires?siret=33135716000075", () => {
+
+        it("should return a 200 status code", (done) => {
+            server.get("/prestataires/?siret=33135716000075")
                 .then((response) => {
                     assert.equal(response.status, 200);
                     done();
@@ -32,7 +104,7 @@ describe("Prestataires Routes", () => {
     describe("GET prestataires/:id", () => {
 
         it("should return a 200 status code", (done) => {
-            server.get("/prestataires/new/1")
+            server.get("/prestataires/1")
                 .then((response) => {
                     assert.equal(response.status, 200);
                     done();
@@ -43,7 +115,7 @@ describe("Prestataires Routes", () => {
         });
 
         it("should return a 404 status code for non-existing value", (done) => {
-            server.get("/prestataires/new/0")
+            server.get("/prestataires/0")
                 .then((response) => {
                     assert.equal(response.status, 404);
                     done();
