c33a9a815f8f1271985c0eb13058b02d4eb479a0
= <aurelien.hugues@supelec.fr>
2017-10-19 14:10:53 +0200

    Update dashboard tests TODO: chose exactly what to do when date is NULL (not yet received)

--- test/backend/routes/test-dashboard.js
+++ test/backend/routes/test-dashboard.js
@@ -30,6 +30,22 @@ describe("Dashboard Routes", () => {
                 })
         });
 
+        it("should have the right total of bordereaux", (done) => {
+            server.get('/dashboard')
+                .then((response) => {
+                    var sum = 0;
+                    response.body.forEach((element) => {
+                        sum += parseInt(element.bordereaux);
+                    })
+                    console.log("total should be 4715, but with NULL dates, it comes to 3914")
+                    assert.equal(sum, 4715);
+                    done();
+                })
+                .catch((error) => {
+                    done(error);
+                })
+        })
+
         it("should have the right quantity", (done) => {
             server.get('/dashboard')
                 .then((response) => {
@@ -37,6 +53,7 @@ describe("Dashboard Routes", () => {
                     response.body.forEach((element) => {
                         sum += parseFloat(element.volume_total);
                     })
+                    console.log("total should be 60358.3682, but with NULL dates, it comes to 58873.9291")
                     assert.equal(sum, 60358.3682);
                     done();
                 })
